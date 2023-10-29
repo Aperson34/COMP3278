0d7c1ad88e3b4633e3bc91f4158ffdd415761955
Zay Chan <zaychan80@gmail.com>
2023-10-29 17:32:52 +0800

    added teacher table and modified some tables

--- database.sql
+++ database.sql
@@ -31,41 +31,63 @@ CREATE TABLE `RegconitionModel` (
 
 # CREATE TABLE 'Courses'
 CREATE TABLE `Courses` (
-  `course_id` varchar(8) NOT NULL,
+  `course_id` int NOT NULL,
+  `course_code` varchar(8) NOT NULL,
   `class` varchar(2) NOT NULL,
   `year_offered` int NOT NULL,
   `course_name` varchar(50) NOT NULL,
-  `welcome_message` varchar(1000) NOT NULL,
-  PRIMARY KEY (`course_id`, `class`, `year_offered`)
+  `welcome_message` varchar(1000),
+  PRIMARY KEY (`course_id`)
 );
 
 #CREATE TABLE 'CourseClass'
 CREATE TABLE `CourseClass` (
-  `course_id` varchar(8) NOT NULL,
-  `class` varchar(2) NOT NULL,
-  `year_offered` int NOT NULL, 
+  `course_id` int NOT NULL,
   `class_date` date NOT NULL,
   `class_time` time NOT NULL,
-  `class_venue` varchar(8) NOT NULL,
-  `zoomlink` varchar(100) NOT NULL,
-  PRIMARY KEY (`course_id`, `class`, `year_offered`, `class_date`, `class_time`)
+  `class_venue` varchar(8),
+  `zoomlink` varchar(100),
+  `is_tutorial` boolean NOT NULL,
+  PRIMARY KEY (`course_id`, `class_date`, `class_time`)
 );
 
 # CREATE TABLE 'CourseMaterial'
 CREATE TABLE `CourseMaterial` (
-  `course_id` varchar(8) NOT NULL,
-  `class` varchar(2) NOT NULL,
-  `year_offered` int NOT NULL,
-  `material_name` varchar(50),
+  `course_id` int NOT NULL,
+  `class_date` date NOT NULL,
+  `class_time` time NOT NULL,
+  `material_name` varchar(50) NOT NULL,
   `file_path` varchar(100) NOT NULL,
-  PRIMARY KEY (`course_id`, `class`, `year_offered`, `material_name`)
+  PRIMARY KEY (`course_id`, `material_name`, `class_date`, `class_time`)
 );
 
 # CREATE TABLE 'ClassTaken'
 CREATE TABLE `ClassTaken` (
   `student_id` int NOT NULL,
-  `course_id` varchar(8) NOT NULL,
-  `class` varchar(2) NOT NULL,
-  `year_offered` int NOT NULL,
-  PRIMARY KEY (`student_id`, `course_id`, `class`, `year_offered`)
+  `course_id` int NOT NULL,
+  PRIMARY KEY (`student_id`, `course_id`)
+);
+
+# CREATE TABLE 'Teachers'
+CREATE TABLE `Teachers` (
+  `teacher_id` int NOT NULL,
+  `teacher_name` varchar(80) NOT NULL,
+  PRIMARY KEY (`teacher_id`)
+);
+
+# CREATE TABLE 'CourseTaught'
+CREATE TABLE `CourseTaught` (
+  `teacher_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  PRIMARY KEY (`teacher_id`, `course_id`)
+);
+
+#CREATE TABLE 'TeacherMessage'
+CREATE TABLE `TeacherMessage` (
+  `teacher_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  `message` varchar(1000) NOT NULL;
+  `message_date` date NOT NULL,
+  `message_time` time NOT NULL,
+  PRIMARY KEY (`teacher_id`, `course_id`, `message_date`, `message_time`)
 );
