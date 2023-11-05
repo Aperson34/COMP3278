03d63730eee3f54b29fbae542cf3c406c32e41ba
Justin Yeung <78725881+justinygtc@users.noreply.github.com>
2023-11-05 14:29:14 +0800

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `Student` (`student_id`, `email`, `name`, `birthday`) VALUES (1, 'justin@hku.hk', 'Justin', '2002-04-02');
+INSERT INTO `Student` (`student_id`, `email`, `name`, `birthday`) VALUES (2, 'zay@hku.hk', 'Zay', '2000-09-12');
