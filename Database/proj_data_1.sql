22a964f7dc9d27e7eb609c2f7d6a18c60764218c
Zay Chan <zaychan80@gmail.com>
2023-11-17 22:51:54 +0800

    changed the sql data and added dummy pdf files for testing

--- Database/proj_data_1.sql
+++ Database/proj_data_1.sql
@@ -1,37 +1,36 @@
-INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('1', "mail@mail.com", "Alix", "1970-01-01");
-INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('2', "mail@mail.com", "Janice", "1970-01-01");
-INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('3', "mail@mail.com", "Jeremy", "1970-01-01");
-INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('4', "mail@mail.com", "Justin", "1970-01-01");
-INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('5', "mail@mail.com", "Zay", "1970-01-01");
+INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('3035788621', "alix@mail.com", "Alix Lee Ka Shing", "2001-10-30");
+INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('3035788622', "janice@connect.hku.hk", "Janice Lui", "2002-05-06");
+INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('3035788623', "jeremy123@outlook.com", "Jeremy Chan", "2003-07-14");
+INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('3035788624', "justin@gmail.com", "Justin NG", "1999-12-09");
+INSERT INTO Student (student_id, email, s_name, birthday) VALUES ('3035788625', "zaychan@connect.hku.hk", "Chan Chin Hung", "2002-08-10");
 
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("1", "01:02:03", "2023-11-02", "05:02:03", "2023-11-02");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("2", "01:02:04", "2023-11-02", "05:02:03", "2023-11-02");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3", "01:02:05", "2023-11-02", "05:02:03", "2023-11-02");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("4", "01:02:06", "2023-11-02", "05:02:03", "2023-11-02");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("5", "01:02:07", "2023-11-02", "05:02:03", "2023-11-02");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("4", "11:22:33", "2023-11-03", "22:33:44", "2023-11-03");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3", "12:34:56", "2023-11-04", "21:44:05", "2023-11-04");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("5", "07:14:23", "2023-11-06", "07:15:11", "2023-11-06");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("5", "21:21:21", "2023-11-06", "22:22:22", "2023-11-07");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("1", "01:02:03", "2023-11-06", "05:02:03", "2023-11-08");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("2", "01:02:03", "2023-11-06", "05:02:03", "2023-11-07");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("4", "01:02:03", "2023-11-07", "05:02:03", "2023-11-07");
-INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3", "01:02:03", "2023-11-08", "05:02:03", "2023-11-08");
+INSERT INTO LoginCredentials (username, pswd, student_id) VALUES ("alix", "alix1030", "3035788621");
+INSERT INTO LoginCredentials (username, pswd, student_id) VALUES ("janice", "jlui0506", "3035788622");
+INSERT INTO LoginCredentials (username, pswd, student_id) VALUES ("jeremy123", "j123714", "3035788623");
+INSERT INTO LoginCredentials (username, pswd, student_id) VALUES ("justin", "j1999", "3035788624");
+INSERT INTO LoginCredentials (username, pswd, student_id) VALUES ("zaychan", "Z@yChan80", "3035788625");
 
-INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("1", "COMP3278", "1A", "2023", "Introduction to database management systems", "Hi");
-INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("2", "COMP3278", "1B", "2023", "Introduction to database management systems", "Hi");
-INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("3", "COMP3230", "1", "2022", "Principles of Operating Systems", "Hi2");
-INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("4", "COMP3314", "2", "2023", "Machine Learning", "Hi3");
+INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3035788621", "10:20:03", "2023-11-02", "12:02:03", "2023-11-02");
+INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3035788622", "13:12:04", "2023-11-13", "14:02:03", "2023-11-13");
+INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3035788623", "23:52:34", "2023-11-16", "00:01:03", "2023-11-17");
+INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3035788624", "05:04:06", "2023-10-24", "05:06:03", "2023-10-24");
+INSERT INTO LoginBehaviour (student_id, login_time, login_date, logout_time, logout_date) VALUES ("3035788625", "23:02:07", "2023-11-16", "23:05:04", "2023-11-16");
+
+
+INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("1", "COMP3278", "1A", "2023", "Introduction to database management systems", "Hi this is introduction to database management system subclass 1A");
+INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("2", "COMP3278", "1B", "2023", "Introduction to database management systems", "Hi this is introduction to database management system subclass 1B");
+INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("3", "COMP3230", "1A", "2022", "Principles of Operating Systems", "Hi2");
+INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("4", "COMP3314", "2B", "2023", "Machine Learning", "Hi3");
 INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("5", "COMP3251", "1A", "2022", "Algorithm design", "Hi4");
 INSERT INTO Courses (course_id, course_code, class_id, year_offered, course_name, welcome_message) VALUES ("6", "COMP3252", "1A", "2022", "Algorithm design and analysis", "Hi4");
 
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-01", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", False);
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-05", "13:30:00", "15:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", False);
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-07", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", False);
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-12", "13:30:00", "15:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", False);
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-14", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", True);
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-19", "13:30:00", "15:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", False);
-INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-21", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/rec/share/AXpuCAAJ_3nxGng3GDBvWYCAcTtRerCOI0rHpLyKCGV_YiM9Y-XYSGdVWDGx_Jg.lubxpz6SgAXZxmiv", True);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-01", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", False);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-05", "13:30:00", "15:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", False);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-07", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", False);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-12", "13:30:00", "15:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", False);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-14", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", True);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-19", "13:30:00", "15:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", False);
+INSERT INTO CourseClass (course_id, class_date, class_time, class_end_time, class_venue, zoomlink, is_tutorial) VALUES ("1", "2023-09-21", "14:30:00", "16:20:00", "MWT1", "https://hku.zoom.us/j/93189128050?pwd=RVZjcnl0anVDbW5OT0EzSXRON3lFdz09", True);
 
 INSERT INTO CourseMaterial (course_id, class_date, class_time, material_name, file_path) VALUES ("1", "2023-09-01", "14:30:00", "intro", "C:\\Users\\admin\\Desktop\\COMP3278\\lec0.pdf");
 INSERT INTO CourseMaterial (course_id, class_date, class_time, material_name, file_path) VALUES ("1", "2023-09-05", "13:30:00", "diagrams1", "C:\\Users\\admin\\Desktop\\COMP3278\\lec1.pdf");
@@ -41,18 +40,18 @@ INSERT INTO CourseMaterial (course_id, class_date, class_time, material_name, fi
 INSERT INTO CourseMaterial (course_id, class_date, class_time, material_name, file_path) VALUES ("1", "2023-09-19", "13:30:00", "sql1", "C:\\Users\\admin\\Desktop\\COMP3278\\lec3.pdf");
 INSERT INTO CourseMaterial (course_id, class_date, class_time, material_name, file_path) VALUES ("1", "2023-09-21", "14:30:00", "sql2", "C:\\Users\\admin\\Desktop\\COMP3278\\lec3.pdf");
 
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("1", "1");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("2", "1");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("3", "1");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("4", "1");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("5", "1");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("1", "3");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("1", "4");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("1", "5");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("2", "2");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("3", "3");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("4", "4");
-INSERT INTO ClassTaken (student_id, course_id) VALUES ("5", "5");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788621", "1");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788622", "1");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788623", "1");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788624", "1");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788625", "1");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788621", "3");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788621", "4");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788621", "5");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788622", "2");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788623", "3");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788624", "4");
+INSERT INTO ClassTaken (student_id, course_id) VALUES ("3035788625", "5");
 
 INSERT INTO Teachers (teacher_id, teacher_name) VALUES ("1", "Ping Luo");
 INSERT INTO Teachers (teacher_id, teacher_name) VALUES ("2", "Peter");
@@ -82,6 +81,6 @@ INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, mess
 INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("5", "4", "Hi4", "2023-08-25", "17:30:00");
 INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("6", "4", "Hi5", "2023-08-25", "18:30:00");
 INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("7", "5", "Hi6", "2023-08-25", "19:30:00");
-INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("8", "5", "E=mc2", "1970-01-01", "00:00:01");
-INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("8", "6", "E=mc2", "1970-01-01", "00:00:01");
-INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("9", "6", "Believe", "1970-01-01", "00:00:01");
+INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("8", "5", "E=mc2", "2023-11-01", "00:00:01");
+INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("8", "6", "E=mc2", "2023-10-010", "00:00:01");
+INSERT INTO TeacherMessage (teacher_id, course_id, t_message, message_date, message_time) VALUES ("9", "6", "Believe", "2023-11-17", "00:00:01");
