ddba93647742f9573dc1921d3bc7c8c4b652ad0b
Zay Chan <zaychan80@gmail.com>
2023-11-19 11:27:41 +0800

    changed getLoginBehaviour from returning one record to all records

--- Database/Backend.py
+++ Database/Backend.py
@@ -132,10 +132,10 @@ def getCourseInfo(course_id): #tested
 def getLoginBehaviour(student_id):
   mycursor.execute(f"SELECT * FROM LoginBehaviour WHERE student_id = {student_id}") #input instructions
   myresult = mycursor.fetchall()
-  login_time = myresult[0][1]
-  login_date = myresult[0][2]
-  logout_time = myresult[0][3]
-  logout_date = myresult[0][4]
+  login_time = myresult[:][1]
+  login_date = myresult[:][2]
+  logout_time = myresult[:][3]
+  logout_date = myresult[:][4]
   return (login_time, login_date, logout_time, logout_date)
 
 def getCourseTeacher(course_id):
