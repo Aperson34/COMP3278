65f1893380a390c47af6c26d8a3f1a2b25121fe1
Aperson34 <jeremy20030425@gmail.com>
2023-11-21 20:15:50 +0800

    bug fix

--- Database/Backend.py
+++ Database/Backend.py
@@ -12,7 +12,7 @@ class Backend(object):
     super().__init__()
     self.mydb = mysql.connector.connect(host="localhost", user="root", password="1989") #change the password
     self.mycursor = self.mydb.cursor()
-    self.createicms()
+    # self.createicms()
     self.mycursor.execute("USE GROUP19ICMS;")
 
   def createicms(self):
